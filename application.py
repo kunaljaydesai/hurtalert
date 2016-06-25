6843ad34c3e06c0377bfb98fd69d5d9f0f79e2ed
System Administrator <root@Kunals-MacBook-Pro-2.local>
2016-06-25 14:58:16 -0700

    done

--- application.py
+++ application.py
@@ -53,6 +53,23 @@ def emergency():
 	report = Reports(time, user, latitude, longitude, type_report)
 	return jsonify(report=report.insert_into_db())
 
+@application.route('/filter')
+def filter():
+	time_start = request.arg.get('time_start')
+	time_end = request.args.get('time_end')
+	center_lat = request.args.get('center_lat')
+	center_lon = request.args.get('center_lon')
+	height = request.args.get('height')
+	width = request.args.get('width')
+	top_left = [center_lat - height / 2, center_lon - width / 2]
+	bottom_right = [center_lat + height / 2, center_lon + height / 2]
+	filter_location_query = Reports.query.filter(Reports.latitude >= top_left[0]).filter(Reports.latitude <= bottom_right[0]).filter(Reports.longitude <= top_left[1]).filter(Reports.longitude >= bottom_right[1]).all()
+	and_time = filter_location_query.filter(Reports.time >= time_start).filter(Reports.time <= time_end)
+	report_list = and_time.all()
+	response_reports = []
+	for reports in report_list:
+		response_reports.append(reports.serialize)
+	return jsonify(reports=response_reports)
 
 if __name__ == "__main__":
 	application.run(host="0.0.0.0", port=80, debug=True, threaded=True)
